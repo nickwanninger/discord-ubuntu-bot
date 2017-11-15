b9350dcafb6e8ac6dde38710cb73f846535a369b
Nick Wanninger <nickwanninger@gmail.com>
2017-11-15 15:48:14 -0600

    fixed me having the key on github:

--- src/index.js
+++ src/index.js
@@ -33,25 +33,6 @@ client.on('message', async message => {
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
 	const command = args.shift().toLowerCase()
 
-	if (command === 'purgeplz') {
-		// This command removes all messages from all users in the channel, up to 100.
-		message.delete().catch(O_o => {})
-		// get the delete count, as an actual number.
-		const deleteCount = parseInt(args[0], 10)
-
-		// Ooooh nice, combined conditions. <3
-		if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-			return message.reply('Please provide a number between 2 and 100 for the number of messages to delete')
-
-		// So we get our messages, and delete them. Simple enough, right?
-		const fetched = await message.channel.fetchMessages({ count: deleteCount })
-		message.channel
-			.bulkDelete(fetched)
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`))
-
-		return
-	}
-
 	if (message.content.indexOf('$') == 0) {
 		const handler = new LinuxCommandRunner()
 		handler.run(client, message)
