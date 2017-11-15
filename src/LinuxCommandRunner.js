d0e238b63aea9b5a379fccfd76ce7eb6c208d734
Nick Wanninger <nickwanninger@gmail.com>
2017-11-15 11:33:04 -0600

    update

--- src/LinuxCommandRunner.js
+++ src/LinuxCommandRunner.js
@@ -16,16 +16,6 @@ class Stdout extends stream.Writable {
 	}
 }
 
-function chunk(str, size) {
-	var chunks = []
-
-	for (var i = 0, charsLength = str.length; i < charsLength; i += size) {
-		chunks.push(str.substring(i, i + size))
-	}
-
-	return chunks
-}
-
 class LinuxCommandRunner {
 	constructor() {}
 	async run(client, message) {
@@ -41,19 +31,45 @@ class LinuxCommandRunner {
 					killingTimeout
 				const outStream = new Stdout()
 				const errStream = new Stdout()
-
-				docker.run('ubuntu-custom', command.split(' '), [ outStream, errStream ], { Tty: false }, function(err, data, container) {
+				// Run a docker container called "ubuntu-custom"
+				docker
+					.run('ubuntu-custom', [ 'bash', '-c', command ], outStream, { Tty: true }, function(
+						err,
+						data,
+						container
+					) {
+						// make sure to stop the timeout function that would run and kill the container.
 						clearTimeout(killingTimeout)
+						// Check if there is an error or not
 						if (err) {
+							// change the initial message to display the status
 							initialmessage.edit('Something went wrong!')
-							message.channel.send("```" + JSON.stringify(err, null, 2) + "```")
+							// display the errors in a code block.
+							// TODO: Display this in a nicer format
+							message.channel.send('```' + JSON.stringify(err, null, 2) + '```')
 						} else {
+							// make sure the process hasn't been killed yet
+							// this prevents the stdout being sent on very long running tasks
 							if (!killedFlag) {
+								// grab the current text from stdout from the sdtOutStream
 								const stdout = outStream.getContent()
-	
+								// Change the initial message to display the new status
+								// and tell the user that the command ran as well as how long it took.
 								initialmessage.edit('Ran `' + command + '` in ' + (Date.now() - start) + 'ms')
-								const splits = chunk(stdout, 1990)
-	
+								// This one is a little tricky.
+								// discord only allows messages with a max size of 2000 characters
+								// so I need to chunk it up into smaller parts. To be safe, I'm cutting
+								// it into 1990 characters so I can include any formatting I might want
+								// Here, I define the initial values
+								const splits = []
+								const max_size = 1990
+
+								// I then loop over and chunk it into seperate bits
+								for (var i = 0, charsLength = stdout.length; i < charsLength; i += max_size) {
+									// push to the splits array.
+									splits.push(stdout.substring(i, i + max_size))
+								}
+								// send each part of the split
 								splits.forEach(s => {
 									message.channel.send('```' + s + '```')
 								})
--- src/index.js
+++ src/index.js
@@ -30,24 +30,24 @@ client.on('ready', () => {
 client.on('message', async message => {
 	if (message.author.bot) return
 
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+	const command = args.shift().toLowerCase()
 
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
-
-	if (command === "purgeplz") {
+	if (command === 'purgeplz') {
 		// This command removes all messages from all users in the channel, up to 100.
-    
+		message.delete().catch(O_o => {})
 		// get the delete count, as an actual number.
-		const deleteCount = parseInt(args[0], 10);
-		
+		const deleteCount = parseInt(args[0], 10)
+
 		// Ooooh nice, combined conditions. <3
-		if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-		return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-		
+		if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+			return message.reply('Please provide a number between 2 and 100 for the number of messages to delete')
+
 		// So we get our messages, and delete them. Simple enough, right?
-		const fetched = await message.channel.fetchMessages({count: deleteCount});
-		message.channel.bulkDelete(fetched)
-		.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+		const fetched = await message.channel.fetchMessages({ count: deleteCount })
+		message.channel
+			.bulkDelete(fetched)
+			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`))
 
 		return
 	}
@@ -58,5 +58,4 @@ client.on('message', async message => {
 		return
 	}
 })
-
 client.login(config.token)
